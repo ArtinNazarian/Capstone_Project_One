c3a7c62f5c778d415ae7b129c5b749d46de5c534
Artin <artin.n31@gmail.com>
2023-06-24 19:17:16 -0700

    updated app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///recipe_db'))
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", False)
+app.config['SQLALCHEMY_ECHO'] = os.environ.get("SQLALCHEMY_ECHO", True)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = os.environ.get("DEBUG_TB_INTERCEPT_REDIRECTS", False)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "happydaysnone1234")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
